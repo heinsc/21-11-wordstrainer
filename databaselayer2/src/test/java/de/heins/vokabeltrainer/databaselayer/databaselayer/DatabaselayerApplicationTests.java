d3faf93def57b7230cf69ad64bb28d4e165bbb88
Carsten Heins <heinsc@t-online.de>
2021-12-01 20:38:49 +0100

    initial

---
+++ databaselayer2/src/test/java/de/heins/vokabeltrainer/databaselayer/databaselayer/DatabaselayerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heins.vokabeltrainer.databaselayer.databaselayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaselayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
